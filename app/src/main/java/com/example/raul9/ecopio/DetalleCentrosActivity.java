3d106ea477546225000644998ef90866df9c8281
Oto80557 <hectorsalasarias@gmail.com>
2017-06-12 21:56:38 -0600

    vista detalle de centros

---
+++ app/src/main/java/com/example/raul9/ecopio/DetalleCentrosActivity.java
@@ -0,0 +1,13 @@
+package com.example.raul9.ecopio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleCentrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_centros);
+    }
+}
